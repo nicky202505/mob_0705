95b244d4a2e3d83f5948ff2cea9f19fadb69c613
nicky202505 <jangnicole878@gmail.com>
2025-07-20 10:58:03 +0900

    20th

--- docs/script/js.js
+++ docs/script/js.js
@@ -1,4 +1,8 @@
 document.querySelector('button').addEventListener('click',function(){
-document.querySelector('.popup').style.display='none' ; 
+document.querySelector('.popup').style.display='none' ;  
+}) 
 
-})
+
+document.getElementById('arrowScroll').addEventListener('mouseenter', function(e) {
+  window.scrollBy({ top: 900, left: 0, behavior: 'smooth' });
+});
